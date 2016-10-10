a5490fddf2c6655f95ad6ac492efbcd66ddea7b8
aditya <adityanant@gmail.com>
2016-10-10 22:22:23 +0530

    ocr

--- app/src/main/java/com/example/arvind/libo/add_book_main.java
+++ app/src/main/java/com/example/arvind/libo/add_book_main.java
@@ -1,7 +1,9 @@
 package com.example.arvind.libo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class add_book_main extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class add_book_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book_main);
+
+
+    }
+    public void goTo_ocr(View view){
+        Intent intent = new Intent (this,ocr.class);
+        startActivity(intent);
     }
 }
